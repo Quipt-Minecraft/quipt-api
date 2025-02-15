082a934f934cce3e2d1120ef87a5290f3bd7c7ae
QuickScythe <QuickScythe@gmail.com>
2025-02-15 01:51:32 -0600

    test before deploy

--- src/test/java/com/quiptmc/api/tests/ApiTests.java
+++ src/test/java/com/quiptmc/api/tests/ApiTests.java
@@ -1,4 +1,18 @@
 package com.quiptmc.api.tests;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ApiTests {
+
+    @Test
+    public void testAlwaysPass() {
+        assertTrue(true);
+        assertFalse(false);
+        assertNull(null);
+        assertNotNull(new Object());
+        assertEquals(1, 1);
+
+    }
 }
