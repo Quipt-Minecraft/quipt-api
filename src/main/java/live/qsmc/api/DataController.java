38f5a04d1b76b75852ccb631836db54657898e73
Quick <quickscythe@gmail.com>
2026-04-24 02:07:55 -0500

    refactor DataController to schedule async update task and return JSON response

--- src/main/java/live/qsmc/api/DataController.java
+++ src/main/java/live/qsmc/api/DataController.java
@@ -1,18 +1,26 @@
 package live.qsmc.api;
 
+import live.qsmc.core2.utils.TaskScheduler;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.MediaType;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/data")
 class DataController {
 
     @GetMapping(value = "/update", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Greeting update() {
-        System.exit(0);
-        return new Greeting();
+    public JSONObject update() {
+        TaskScheduler.scheduleAsyncTask(() -> {
+            System.exit(0);
+        },2, TimeUnit.SECONDS);
+        JSONObject json = new JSONObject();
+        json.put("message", "Update in progress...");
+        return json;
     }
 
     @RequestMapping("/download")
