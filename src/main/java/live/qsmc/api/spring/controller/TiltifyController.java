6eb9ff4943e8a8beb4192783d6d63e23c3814f8c
Quick <quickscythe@gmail.com>
2026-05-15 18:28:14 -0500

    Add TiltifyController for handling verification requests

---
+++ src/main/java/live/qsmc/api/spring/controller/TiltifyController.java
@@ -0,0 +1,37 @@
+package live.qsmc.api.spring.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import live.qsmc.api.QuiptApiApplication;
+import live.qsmc.api.util.Utils;
+import live.qsmc.quipt.core.utils.net.ApiResponse;
+import org.json.JSONObject;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.HandlerMapping;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/tiltify")
+class TiltifyController {
+
+    @RequestMapping(value = "/verify", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResponse<?> verify() {
+        return new ApiResponse<>(ApiResponse.Status.SUCCESS, "Tiltify verification successful");
+    }
+
+}
+
